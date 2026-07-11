62ff57a5525aa17c701056d867dc6543c63263d9
Mosarof Hossain <83153303+TechFahim3@users.noreply.github.com>
2026-07-11 14:03:40 +0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,3 +1,17 @@
+from flask import Flask
+from threading import Thread
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Bot is running!"
+
+def run():
+    app.run(host='0.0.0.0', port=10000)
+
+t = Thread(target=run)
+t.start()
+
 import os
 import logging
 import sqlite3
